f5dc8ad332302da13a7abddbc08a4f799517b019
BartekWons <bartek.wons@gmail.com>
2024-11-06 22:01:52 +0100

    add ffmpeg wrapper

---
+++ FormatConverter/FormatConverter/MVVM/Model/Wrapper.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace FormatConverter.MVVM.Model
+{
+    public class Wrapper
+    {
+        public static void Convert(string filePath, string outputPath)
+        {
+            string ffmpegCommand = @$"-i {filePath} {outputPath}";
+            var processStartInfo = new ProcessStartInfo()
+            {
+                FileName = "ffmpeg",
+                Arguments = ffmpegCommand,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            using var process = new Process();
+            process.StartInfo = processStartInfo;
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+        }
+    }
+}
--- FormatConverter/FormatConverter/MVVM/ViewModel/MainWindowViewModel.cs
+++ FormatConverter/FormatConverter/MVVM/ViewModel/MainWindowViewModel.cs
@@ -1,11 +1,9 @@
-﻿using Accessibility;
-using FormatConverter.Core;
+﻿using FormatConverter.Core;
 using FormatConverter.MVVM.Model;
 using Microsoft.Win32;
-using System.Diagnostics;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Data;
 
 namespace FormatConverter.MVVM.ViewModel
 {
@@ -14,12 +12,17 @@ namespace FormatConverter.MVVM.ViewModel
         public RelayCommand DragWindowCommand { get; set; }
         public RelayCommand CloseCommand { get; set; }
         public RelayCommand MinimizeCommand { get; set; }
-        public RelayCommand ChooseAudioFile { get; set; }
+        public RelayCommand ChooseAudioFileCommand { get; set; }
+        public RelayCommand ChooseDestinationPathCommand { get; set; }
+        public RelayCommand ExecuteCommand { get; set; }
 
         private MainFilesExtensions _mainFilesExtensions = new();
 
         public List<string> AvailableExtensions { get; set; }
 
+        private string _filePath;
+        private string _destinationPath;
+        private string _fileName;
 
         public string SelectedExtension
         {
@@ -27,11 +30,18 @@ namespace FormatConverter.MVVM.ViewModel
             set { OnPropertyChanged(); }
         }
 
+        private string _selectedExtensionFromFile;
+
         public string SelectedExtensionFromFile
         {
-            get { return null; }
-            set { OnPropertyChanged(); }
+            get { return _selectedExtensionFromFile; }
+            set 
+            { 
+                _selectedExtensionFromFile = value;
+                OnPropertyChanged();
+            }
         }
+
 
         public int SelectedIndex
         {
@@ -62,21 +72,65 @@ namespace FormatConverter.MVVM.ViewModel
                 Application.Current.MainWindow.WindowState = WindowState.Minimized;
             });
 
-            ChooseAudioFile = new RelayCommand(o =>
+            ChooseAudioFileCommand = new RelayCommand(o =>
             {
                 var dialog = new OpenFileDialog
                 {
                     Filter = "ALL|*.mp3;*.wav;*.ogg;*.flac;",
                     Multiselect = false
                 };
-                if (dialog.ShowDialog() == true)
-                {
-                    Debug.WriteLine(dialog.SafeFileName);
-                    string extension = Path.GetExtension(dialog.SafeFileName);
-                    Debug.WriteLine(extension);
-                    SelectedExtensionFromFile = extension;
-                }
+                if (dialog.ShowDialog() == false)
+                    return;
+                _filePath = dialog.FileName;
+                ShowExtensionOnScreen(dialog.SafeFileName);
             });
+
+            ChooseDestinationPathCommand = new RelayCommand(o =>
+            {
+                var dialog = new OpenFolderDialog()
+                {
+                    Multiselect = false,
+                    InitialDirectory = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @"\Downloads\")
+                };
+                if (dialog.ShowDialog() == false)
+                    return;
+                _destinationPath = dialog.FolderName;
+            });
+
+            ExecuteCommand = new RelayCommand(o =>
+            {
+                if (String.IsNullOrEmpty(_filePath) || String.IsNullOrEmpty(_destinationPath))
+                {
+                    MessageBox.Show("Choose existing audio file or destination folder.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                if (AreTheSameExtensions())
+                {
+                    MessageBox.Show("Conversion to the same format.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                Wrapper.Convert(_filePath, Path.Combine(_destinationPath, $"{_fileName}.{_mainFilesExtensions.SelectedExtension}"));
+                MessageBox.Show("Succesfull conversion.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            });
+        }
+
+        private void ShowExtensionOnScreen(string path)
+        {
+            string fileNameWithExtension = Path.GetFileName(path);
+            var matches = Regex.Match(fileNameWithExtension, @"(.*)\.(\w*)");
+            if (matches.Success)
+            {
+                _fileName= matches.Groups[1].Value;
+                SelectedExtensionFromFile = matches.Groups[2].Value;
+            }
+        }
+
+        private bool AreTheSameExtensions()
+        {
+            var matches = Regex.Match(Path.GetExtension(_filePath), @".(\w*)");
+            if (!matches.Success)
+                return false;
+            return _mainFilesExtensions.SelectedExtension.Equals(matches.Groups[1].Value);
         }
     }
 }
